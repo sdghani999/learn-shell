84a64286563d8652e7449e169ad9f861d1b19d6a
ghani <sd.ghani999@gmail.com>
2023-05-28 18:36:48 +0530

    install nginx sctipt

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,8 @@ echo -e "\e[32mremove the content \e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[32mdownlode the frontend content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log 2>/tmp/error.log
+
 
 echo -e "\e[32mcopy the frontend content \e[0m"
 cd /usr/share/nginx/html >/tmp/roboshop.log

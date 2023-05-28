3f977afeea7dde5e23d2a756c993c43010d3a923
ghani <sd.ghani999@gmail.com>
2023-05-28 11:27:21 +0530

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,13 @@
+echo "\e[32minstall nginx server \e[0m "
 yum install nginx -y
+echo "\e[32mremove the content \e[0m"
 rm -rf /usr/share/nginx/html/*
+echo "\e[32mdownlode the content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo "\e[32mcopy the content \e[0m"
 cd /usr/share/nginx/html
+echo "\e[32mextract the content \e[0m"
 unzip /tmp/frontend.zip
+echo "\e[32mstart the server \e[0m"
 systemctl enable nginx
 systemctl restart nginx

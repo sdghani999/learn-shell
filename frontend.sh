8ea2485ce45ad4edfcfd5d640ed74fd1ebb8e75d
ghani <sd.ghani999@gmail.com>
2023-06-18 21:52:23 +0530

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,5 @@
 source common.sh
+component=frontend
 
 echo -e "${colour}install nginx server ${nocolour}"
 yum install nginx -y &>>/tmp/roboshop.log
@@ -8,16 +9,16 @@ echo -e "${colour}remove the content ${nocolour}"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "${colour}downlode the frontend content ${nocolour}"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+echo -e "${colour}downlode the ${component} content ${nocolour}"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>/tmp/roboshop.log
 stat_check $?
 
-echo -e "${colour}copy the frontend content ${nocolour}"
+echo -e "${colour}copy the ${component} content ${nocolour}"
 cd /usr/share/nginx/html &>>/tmp/roboshop.log
 stat_check $?
 
 echo -e "${colour}extract the frotend content ${nocolour}"
-unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+unzip /tmp/${component}.zip &>>/tmp/roboshop.log
 stat_check $?
 
 echo -e "${colour}start nginx server ${nocolour}"

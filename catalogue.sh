ba9489fe3a2f5f5e2373cfdbd6293ffcf72604d7
ghani <sd.ghani999@gmail.com>
2023-06-18 22:03:31 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,5 @@
 source common.sh
+component=catalogue
 
 echo -e "${colour}configure nodejs repo ${nocolour}"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
@@ -11,32 +12,32 @@ useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "${colour}configure application Directory ${nocolour}"
 rm -rf/ app &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
+mkdir ${app-path} &>>/tmp/roboshop.log
 
 echo -e "${colour}downlode application content ${nocolour}"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
-cd /app
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>/tmp/roboshop.log
+cd ${app-path}
 
 echo -e "${colour}extract application content ${nocolour}"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
-cd /app
+unzip /tmp/${component}.zip &>>/tmp/roboshop.log
+cd ${app-path}
 
 echo -e "${colour} install nodejs  dependencies ${nocolour}"
 npm install &>>/tmp/roboshop.log
 
-echo -e "${colour}configure catalogue srevice ${nocolour}"
-cd catalogue.service/etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+echo -e "${colour}configure ${component} srevice ${nocolour}"
+cd ${component}.service/etc/systemd/system/${component}.service &>>/tmp/roboshop.log
 
 
-echo -e "${colour}start catalogue service ${nocolour}"
+echo -e "${colour}start ${component} service ${nocolour}"
 systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+systemctl enable ${component} &>>/tmp/roboshop.log
+systemctl restart ${component} &>>/tmp/roboshop.log
 
 echo -e "${colour}install mongodb clint ${nocolour}"
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "${colour} Load Schema ${nocolour}"
-mongo --host mongodb-dev.devopsb73.online </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mongodb-dev.devopsb73.online <${app-path}/schema/${component}.js &>>/tmp/roboshop.log
 
 
